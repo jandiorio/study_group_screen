91b4481d0a02129cb84c4ddb9bc44849d2d98138
daladen <daladen@users.noreply.github.com>
2020-06-04 21:50:33 -0700

    adds my python script

---
+++ dan_laden.py
@@ -0,0 +1,13 @@
+
+import yaml
+
+list = [{
+    "company": "WWT",
+    "email": "daniel.ladem@wwt.com",
+    "first_name": "Dan",
+    "github_username": "daladen",
+    "last_name": "Laden"
+}]
+
+with open(r'dan_laden.yaml', 'w') as file:
+    documents = yaml.dump(list, file)
