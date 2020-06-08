c3bb183b11e3cf32d18a3bd1dbb4dea54458ca61
AMSJFKKEFLHR <christian.s.horner@gmail.com>
2020-06-08 17:15:34 -0400

    this is the second time

---
+++ christian_horner.py
@@ -0,0 +1,11 @@
+import yaml
+
+dictionary_list = [{
+    "company": "WWT",
+    "email": "christian.horner@wwt.com",
+    "first_name": "Christian",
+    "github_username": "AMSJFKKEFLHR",
+    "last_name": "Horner"
+}]
+
+print yaml.dump(dictionary_list)
