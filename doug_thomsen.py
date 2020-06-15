10eb6bb33bfb8228df376c67f9f01786bd5f71f5
dathomsen <doug.thomsen@wwt.com>
2020-06-15 14:12:25 -0500

    adding my python script

---
+++ doug_thomsen.py
@@ -0,0 +1,20 @@
+#doug_thomsen.py ver 1.0
+#create by Doug Thomsen
+#creates one list with one dictionary inside and prints to yaml file
+
+#importing needed modules
+import yaml
+
+#creating list with one dictionary inside
+listdoug = [
+    {
+        'first_name': 'Doug',
+        'last_name': 'Thomsen',
+        'company': 'WWT',
+        'email': 'doug.thomsen@wwt.com',
+        'github_username': 'dathomsen'
+    }
+        ]
+
+with open('doug_thomsen.yaml', "w") as output:
+    yaml.dump(listdoug, output)
