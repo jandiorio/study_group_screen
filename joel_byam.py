939f669cb18b2556699c74a0afc8d379bed73770
Byam <byamj@wwt.com>
2020-06-15 23:18:03 -0400

    updated python script v2 - fix variable names

--- joel_byam.py
+++ joel_byam.py
@@ -1,8 +1,8 @@
 import yaml
 
 #dictionary entries
-profile_dict ={{'First Name' : ['Joel']},{'Last Name' : ['Byam']}, {'Company' : ['WWT']}, {'email' : ['joel.byam@wwt.com']}, {'github_username' : ['GroovyJ']}}
+profile_dict =[{'First Name' : ['Joel']},{'Last Name' : ['Byam']}, {'Company' : ['WWT']}, {'email' : ['joel.byam@wwt.com']}, {'github_username' : ['GroovyJ87']}]
 
 #write to yaml file
-with open(r'C:\Users\byamj\Documents\Python\yaml\yaml_test_1.yaml', 'w') as file:
-    yaml_test_1 = yaml.dump(multi_list_yaml,file)
+with open(r'C:\Users\byamj\Documents\Python\yaml\joel_byam.yaml', 'w') as file:
+    yaml_output = yaml.dump(profile_dict,file)
