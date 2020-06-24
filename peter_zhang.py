21e342ab379d8ecaa02f1c8fce21808688e62cbb
Peter Zhang <peter.zhang@wwt.com>
2020-06-24 15:30:27 -0400

    restrutured the class so that employee_info is initialized with as an empty list

--- peter_zhang.py
+++ peter_zhang.py
@@ -3,16 +3,19 @@
 import yaml
 import json
 
-class WWTEmployee(object):
+class WWTEmployeeDirectory(object):
     company = 'WWT'
-    def __init__(self, f_name, l_name, email, gh_username):
-        self.employee_info = [{
+    def __init__(self):
+        self.employee_info = []
+
+    def add_employee(self, f_name, l_name, email, gh_username):
+        return self.employee_info.append({
             'Company': self.company,
             'email':email,
             'first_name':f_name,
             'last_name':l_name,
             'github_username': gh_username
-        }]
+        })
 
     def output_yaml(self):
         return yaml.dump(self.employee_info)
@@ -25,7 +28,8 @@ def main():
     l_name = 'Zhang'
     email = 'peter.zhang@wwt.com'
     gh_username = "ACISupport"
-    init_employee = WWTEmployee(f_name, l_name, email, gh_username)
+    employee_directory = WWTEmployeeDirectory()
+    employee_directory.add_employee('peter', 'zhang', 'peter.zhang@wwt.com', 'ACISupport' )
     with open ('employees.yml', 'r+') as yml_file:
         employees = yaml.load(yml_file, Loader=yaml.FullLoader)
         try:
@@ -34,9 +38,9 @@ def main():
                     print (f"Employee with email {email} already exists in the system")
                     break
             else:
-                yml_file.write(f"{init_employee.output_yaml()}")
+                yml_file.write(f"{employee_directory.output_yaml()}")
         except TypeError:
-            yml_file.write(f"{init_employee.output_yaml()}")
+            yml_file.write(f"{employee_directory.output_yaml()}")
 
 if __name__ == "__main__":
     main()
