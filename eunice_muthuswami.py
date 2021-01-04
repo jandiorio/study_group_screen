1f4b3f4e8b8b3664b52bdb2410d09aae1531f215
Eunice Muthuswami <eunice.muthuswami@wwt.com>
2021-01-04 17:02:28 -0500

    adds my python script

---
+++ eunice_muthuswami.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import yaml
+
+# Define Variables
+em_first = 'Eunice'
+em_last = 'Muthuswami'
+em_company = 'WWT'
+em_email = 'eunice.muthuswami@wwt.com'
+em_username = 'eunidc'
+
+# My information in dictionary format
+myinfo = [
+	{
+		'company':em_company,
+		'email':em_email,
+		'first_name':em_first,
+        'github_username':em_username,
+		'last_name':em_last,
+	}
+]
+
+# Write to file in yaml format
+with open('./eunice_muthuswami.yaml', 'w') as my_file:
+    output_file = yaml.dump(myinfo, my_file)
