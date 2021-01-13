60f5a1a18dd2d71c2266aeb8f149a3d44a577f0b
james jackson <james.w.jacksonjr@boeing.com>
2021-01-13 13:58:53 -0600

    modified:   james_jackson.py

--- james_jackson.py
+++ james_jackson.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python
 import yaml
 
-dict = [{'first_name': 'James', 'last_name': 'Jackson', 'company': 'The Boeing Company', 'email': 'james.w.jacksonjr@boeing.com', 'github_username': 'jwjacks' }]
+dict = [ {
+            'first_name': 'James',
+            'last_name': 'Jackson',
+            'company': 'The Boeing Company',
+            'email': 'james.w.jacksonjr@boeing.com',
+            'github_username': 'jwjacks'
+        } ]
 
-f = open('myinfo.yaml', 'w+')
-yaml.dump(dict, f)
+file = open('myinfo.yaml', 'w+')
+yaml.dump(dict, file)
