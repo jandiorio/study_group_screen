ea70638b2f2ecee24d8053c05afd97d00023c7a5
John T <jtdevnet0@gmail.com>
2021-05-11 14:47:47 -0400

    johnnys python script

--- johnny_teixido.py
+++ johnny_teixido.py
@@ -2,7 +2,7 @@
 
 import yaml
 
-johnteixido = [{'first_name': 'Johnny', 'last_name': 'Teixido', 'company': 'Truist', 'email': 'john.teixido@truist.com', 'github_username': 'jt252' }]
+johnteixido = [{'first_name': 'Johnny', 'last_name': 'Teixido', 'company': 'Truist', 'email': 'john.teixido@truist.com', 'github_username': 'JT252' }]
 
 with open('johnnyteixido.yaml', 'w') as jt:
 
