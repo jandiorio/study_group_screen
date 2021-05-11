2dd5d7be95517e40e93149c4471793adc0cd4b50
Bill Thompson <thompsow@Genisys.local>
2021-05-11 08:55:53 -0500

    Bill's study group screen

---
+++ bill_thompson.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import yaml
+
+#Create a dictionary of my information
+
+myinfo = {
+    "company":  "WWT",
+    "email": "bill.thompson@wwt.com",
+    "first_name":  "Bill",
+    "last_name":  "Thompson",
+    "github_username": "wthomps413"
+}
+
+#Convert the dictionary to YAML and output into file 
+
+with open (r'bill_thompson.yaml', 'w') as file:
+     info = yaml.dump(myinfo, file)
