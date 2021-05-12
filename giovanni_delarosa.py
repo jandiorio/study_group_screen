219a1978df4b9042a8030e8b7c4b2246eaf5e513
giodelarosa <giodelarosa@gmail.com>
2021-05-12 10:34:38 -0400

    adds my python script

--- giovanni_delarosa.py
+++ giovanni_delarosa.py
@@ -1,5 +1,6 @@
 import yaml
 
+#Creates a list of dictionary pairs
 readmeList = [ 
     {
         'first_name': 'giovanni', 
@@ -12,7 +13,13 @@ readmeList = [
 
 #print (readmeList)
 
+#prints the dic in yaml format
 print (yaml.dump(readmeList))
+
+
+#Creates a file call yamlFile in the studygroup directory 
+#by openning it and writting the list call readmeList 
+#the list gets written in yaml format using the yaml.dump
 
 with open(r'/Users/giovannidelarosa/study_group_screen/yamlFile.yaml', 'w') as file:
         documents = yaml.dump(readmeList, file)
