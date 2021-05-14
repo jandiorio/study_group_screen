792330b287404747934a203adf9db53af12dba4d
mseitz83 <65318806+mseitz83@users.noreply.github.com>
2021-05-14 08:49:43 -0400

    Update matthew_seitz.py

--- matthew_seitz.py
+++ matthew_seitz.py
@@ -1,7 +1,6 @@
 import yaml
 dict = {'company': 'WWT','email': 'matthew.seitz@wwt.com','first_name': 'Matthew','github_username': 'mseitz83','last_name': 'Seitz'}
 
-with open('matthew_seitz.yaml', 'w') as f:
-    yaml_file = yaml.dump(dict)
-    f.write(str(yaml_file))
-    f.close
+f = open('matthew_seitz.yaml', 'w')
+f.write(yaml.dump(dict))
+f.close()
